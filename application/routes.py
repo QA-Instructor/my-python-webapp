f46f7dcdb6ba577d3f2c576149f9aedd1936afd7
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-05-08 16:11:18 +0100

    Jokes route with 6 jokes

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from application import app
+import random
 
 
 @app.route('/')
@@ -9,6 +10,24 @@ def home():
     return render_template('home.html', title='Home')
 
 
-@app.route('/welcome/<name>')
-def welcome(name):
-    return render_template('welcome.html', name=name, group='Everyone')
+@app.route('/welcome/<string:name>')
+def welcome(name='Team'):
+    return render_template('welcome.html', title="Welcome", name=name.title(), group='Everyone')
+
+
+@app.route('/joke')
+def joke():
+    joke_number = random.randrange(len(joke_dict))
+    joke_question = joke_dict[joke_number][0]
+    joke_answer = joke_dict[joke_number][1]
+    return render_template('joke.html', title="Joke Time", joke_question=joke_question, joke_answer=joke_answer, number_of_jokes=len(joke_dict))
+
+
+joke_dict = {0: ["Why was Cinderella so bad a football?", "She kept running away from the ball!"],
+             1: ["What do you call a pile of cats?", "A meow-ntain"],
+             2: ["Why did the bicycle fall over?", "Because it was two tired"],
+             3: ["What do you call a sad strawberry?", "A blueberry!"],
+             4: ["How do you organise a space party?", "You planet."],
+             5: ["What do cows read the most?", "Cattle-logs."]
+             }
+
